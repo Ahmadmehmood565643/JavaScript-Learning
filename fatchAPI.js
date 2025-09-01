09faee9cc61b2155feefadd0ee47edb8bee85ba6
Ahmad Mehmood <ahmadmehmood565643@gmail.com>
2025-09-01 17:21:49 +0500

    fatch

---
+++ fatchAPI.js
@@ -0,0 +1,20 @@
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+
+async function getData() {
+  const url = "https://jsonplaceholder.typicode.com/todos/1";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+getData();
+
